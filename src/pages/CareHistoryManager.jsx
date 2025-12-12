d8a607a88e168c8e425a8d181159d85787deb7c4
vutran01122003 <tranducvu234@gmail.com>
2025-12-12 20:50:34 +0700

    feat: implement history care

--- src/pages/CareHistoryManager.jsx
+++ src/pages/CareHistoryManager.jsx
@@ -133,7 +133,7 @@ const ProductHistoryManager = () => {
 
     // --- RENDER (GIỮ NGUYÊN UI) ---
     return (
-        <div className="min-h-screen bg-gray-50 p-4 md:p-8 font-sans text-slate-800">
+        <div className="min-h-screen  bg-white p-4 md:p-8 font-sans text-slate-800 rounded-lg">
             <div className="max-w-4xl mx-auto">
                 {/* HEADER */}
                 <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-8 gap-4">
@@ -158,7 +158,7 @@ const ProductHistoryManager = () => {
                                 resetForm();
                                 setIsModalOpen(true);
                             }}
-                            className="bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded-lg flex items-center gap-2 shadow-md transition-all text-sm font-medium"
+                            className="bg-indigo-600 hover:bg-indigo-700 text-white px-4 py-2 rounded-lg flex items-center gap-2 shadow-md transition-all text-sm font-medium"
                         >
                             <Plus size={18} /> <span className="hidden sm:inline">Thêm mới</span>
                         </button>
@@ -193,7 +193,7 @@ const ProductHistoryManager = () => {
                                     </div>
 
                                     {/* CARD */}
-                                    <div className="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] bg-white p-5 rounded-xl shadow-sm border border-gray-100 hover:shadow-md transition-shadow ml-auto md:ml-0">
+                                    <div className="w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] bg-gray-50 p-5 rounded-xl shadow-sm border border-gray-100 hover:shadow-md transition-shadow ml-auto md:ml-0">
                                         <div className="flex justify-between items-start mb-2">
                                             <div className="flex items-center gap-2">
                                                 <span
